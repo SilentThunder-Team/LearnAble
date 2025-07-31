a2cedc4b9a2038626b5d21ae5a912baa602a512f
OmSingh04 <omsingh18609@gmail.com>
2025-08-01 01:48:14 +0530

    setting up the frontend environment using react-vite and tailwind css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='bg-indigo-500'>
+      <h1 className='text-white'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
